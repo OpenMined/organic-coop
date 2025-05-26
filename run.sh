49b9c9f788ca77ffc44f3678511d276b86b22885
rasswanth-s <43314053+rasswanth-s@users.noreply.github.com>
2025-05-26 15:55:46 +0530

    Added ability to start rds app in background

--- run.sh
+++ run.sh
@@ -5,6 +5,9 @@ uv venv -p 3.12
 uv pip install -r requirements.txt
 
 
+# Start the RDS Server in Background
+uv run syft-rds server &
+
 # Set default port if not provided
 SYFTBOX_ASSIGNED_PORT=${SYFTBOX_ASSIGNED_PORT:-8080}
 uv run uvicorn server.main:app --reload --host 0.0.0.0 --port $SYFTBOX_ASSIGNED_PORT
